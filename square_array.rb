5cd0894e02c2df7994fa5be8ef6f7f68c5853758
CraigSmith789 <craig789@gmail.com>
2020-10-21 21:15:40 -0400

    commit tree 994473a5867df13740f3e1e9afe0f71c57c2c56f

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |x|
+  sqr = x**2
+  new_array.push(sqr)
+  end
+  new_array
+  # array.each do |x|
+  #   new_array = [x**2]
+    
+  # end
 end
